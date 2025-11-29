70f8a4d6d1dc2b7c1630e4463d624734ad1355fc
Abduraxmon-st <2442abd72@gmail.com>
2025-11-29 12:25:13 +0500

    slug bug fixed

--- src/app/portfolio/[slug]/page.tsx
+++ src/app/portfolio/[slug]/page.tsx
@@ -1,5 +1,5 @@
-export default function PortfolioDetailPage({ params }: { params: { slug: string } }) {
-  console.log("Slug:", params.slug);
-  
-  return <div>{params.slug}</div>;
+export default async function PortfolioDetailPage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
+
+  return <div>Portfolio page: {slug}</div>;
 }
