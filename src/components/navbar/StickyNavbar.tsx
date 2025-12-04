4e24b6a9c5baf24a210ea19189df22469cadf10e
Abduraxmon-st <2442abd72@gmail.com>
2025-12-04 18:05:50 +0500

    /

--- src/components/navbar/StickyNavbar.tsx
+++ src/components/navbar/StickyNavbar.tsx
@@ -43,12 +43,12 @@ export const StickyNavbar = ({
   }, []);
 
   return (
-    <div ref={container} className="fixed max-w-[450px] w-full sm:w-[92%] xl:w-full top-0 left-0 sm:left-1/2 sm:-translate-x-1/2 z-10 px-4 sm:px-0" >
+    <div ref={container} className={`fixed max-w-[450px] w-full sm:w-[92%] xl:w-full left-0 sm:left-1/2 sm:-translate-x-1/2 z-10 px-4 sm:px-0 transition-all duration-300 ${visible ? "top-0" : "-top-25"}`} >
       <div data-swapy-slot="navbar" className="mx-auto w-full mt-4 sm:mt-5">
         <div
           ref={swapy}
           data-swapy-item="navbar"
-          className={`overflow-hidden w-full flex items-center justify-between py-3 xl:py-4 px-5 xl:px-6 border border-borderColor rounded-[14px] xl:rounded-2xl bg-black/60 navbar-shadow select-none backdrop-blur-[2px]  `}
+          className={`overflow-hidden w-full flex items-center justify-between py-3 xl:py-4 px-5 xl:px-6 border border-borderColor rounded-[14px] xl:rounded-2xl bg-black/60 navbar-shadow select-none backdrop-blur-[2px]`}
         >
           {isHome ? (
             <Logo />
