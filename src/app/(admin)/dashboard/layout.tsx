b6442264919fdfa6e4bd5d4d8a4f22e13f3c23c7
Abduraxmon-st <2442abd72@gmail.com>
2026-03-10 13:10:27 +0500

    .

--- src/app/(admin)/dashboard/layout.tsx
+++ src/app/(admin)/dashboard/layout.tsx
@@ -9,7 +9,7 @@ const DashboardLayout = ({
 }>) => {
   return (
     <div className='flex gap-3 lg:gap-4 min-h-dvh max-h-dvh h-dvh min-w-screen w-screen max-w-screen py-3 lg:py-4 px-3 overflow-hidden'>
-      <DashboardSidebar className='hidden md:block bg-borderColor/50 rounded-md lg:rounded-xl text-descColor!' />
+      <DashboardSidebar className='hidden md:flex bg-borderColor/50 rounded-md lg:rounded-xl text-descColor!' />
       <div style={{ scrollbarWidth: "none" }} className='flex-1 flex flex-col gap-3 lg:gap-4'>
         <AdminNavbar />
         <div className="overflow-auto bg-borderColor/50 text-descColor! rounded-md lg:rounded-xl p-4 flex-1">
