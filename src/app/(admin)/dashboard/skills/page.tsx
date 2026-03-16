99db52b64b799d829c712b0e80314ebcfa581880
Abduraxmon-st <2442abd72@gmail.com>
2026-03-16 14:21:08 +0500

    tools added

--- src/app/(admin)/dashboard/skills/page.tsx
+++ src/app/(admin)/dashboard/skills/page.tsx
@@ -1,15 +1,18 @@
 "use client"
 import { TechnologyCard } from "@/components/admin/card"
-import { SkillsModal } from "@/components/admin/modal"
+import { ProjectModal, SkillsModal } from "@/components/admin/modal"
 import { SearchInput } from "@/components/admin/search"
+import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
-import { technology } from "@/data/skills"
+import { technology, tools } from "@/data/skills"
 import { Technology } from "@/types"
-import { Plus } from "lucide-react"
+import { Plus, X } from "lucide-react"
 import { useEffect, useState } from "react"
 
 const SkillsPage = () => {
   const [open, setOpen] = useState<boolean>(false)
+  const [openEdit, setOpenEdit] = useState(false)
+  const [toolI, setTool] = useState<string | null>(null)
   const [tech, setTech] = useState<Technology | null>(null)
   const [search, setSearch] = useState<string>("")
   const [debouncedSearch, setDebouncedSearch] = useState("");
@@ -56,6 +59,53 @@ const SkillsPage = () => {
                 </div>
               ))
           }
+        </div>
+      </div>
+      <div className="mt-4">
+        <p className="text-lg mb-2">Tools</p>
+        <div className="flex flex-wrap gap-4 bg-mainColor/40 p-3 rounded-md">
+          {
+            tools.map((tool) => (
+              <div
+                className="relative group"
+                key={tool}
+                onClick={() => {
+                  setTool(tool)
+                  setOpenEdit(true)
+                }}
+              >
+                <Badge
+                  className="hover:bg-cyan-700/30 cursor-pointer"
+                >
+                  {
+                    (openEdit && (toolI === tool)) ?
+                      <input type="text" className="ring-0 outline-0 border-0 w-max max-w-30" />
+                      : tool
+                  }
+                </Badge>
+                <button
+                  type="button"
+                  onClick={(e) => {
+                    e.stopPropagation()
+                    if (!openEdit) {
+                      setTool(tool)
+                      console.log("Deleted tool:", tool);
+                    } else {
+                      setOpenEdit(false)
+                      setTool(null)
+                    }
+                  }}
+                  className="hidden group-hover:block absolute rounded-full p-px bg-red-500/20 border border-red-600 z-3 right-0 -top-0.5 cursor-pointer">
+                  <X size={8} className="text-red-500" />
+                </button>
+              </div>
+            ))
+          }
+          <Badge
+            className="hover:bg-descColor/20 border-borderColor text-descColor bg-descColor/10 [&>svg]:size-4 cursor-pointer"
+          >
+            <Plus /> add tool
+          </Badge>
         </div>
       </div>
       <SkillsModal
